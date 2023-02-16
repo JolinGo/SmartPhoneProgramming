47a28a513c3237e362c5271908e6f090af4b61f3
Zhuolin Wang <wangzlgood@163.com>
2023-02-16 12:12:02 -0800

    Checking in code for CustomTableViewCell

---
+++ CustomTableViewCell/CustomTableViewCell/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  CustomTableViewCell
+//
+//  Created by Zhuolin Wang on 2023/2/16.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imgContainer: UIImageView!
+    
+    @IBOutlet weak var lblImage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
---
+++ CustomTableViewCell/CustomTableViewCell/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CustomTableViewCell
+//
+//  Created by wzl on 2023/2/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let imageNames = ["Food0", "Food1", "Food2", "Food3", "Food4", "Food5", "Food6", "Food7", "Food8", "Food9"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        cell.imgContainer.image = UIImage(named: imageNames[indexPath.row])
+        cell.lblImage.text = imageNames[indexPath.row]
+        
+        return cell
+    }
+    
+
+}
+
